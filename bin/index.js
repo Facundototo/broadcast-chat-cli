a46562be7210b705a22be579dcfc0a003757514a
Facundototo <facutoto666@gmail.com>
2025-01-15 16:01:23 -0300

    starting

---
+++ bin/index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+import { connectToServer } from "../lib/client.js";
+import { startServer } from "../lib/server.js";
+
+const args = process.argv.slice(2);
+
+switch(args[0]){
+    case 'start':
+        startServer();
+        break;
+    case 'connect':
+        connectToServer();
+        break;
+}
