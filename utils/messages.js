f85c33e70978394f89b57d3ed5744864e22e6bf7
Facundototo <facutoto666@gmail.com>
2025-01-20 14:37:11 -0300

    stores in messages.js the words to be used in the program

---
+++ utils/messages.js
@@ -0,0 +1,6 @@
+export const {
+    CLIENT_TYPE = '-> ',
+    SERVER_SHUTTING_DOWN = '** server shutting down **',
+    CLIENT_CLOSED = '\n--- client closed ---',
+    CLIENT_CONNECTED = '\n--- connected to the chat ---'
+} = process.env
