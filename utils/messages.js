83d94d5b3a063d17ff32d21908b1b1b871a83fbe
Facundototo <facutoto666@gmail.com>
2025-01-20 16:31:52 -0300

    adding prompts

--- utils/messages.js
+++ utils/messages.js
@@ -1,6 +1,7 @@
 export const {
-    CLIENT_TYPE = '-> ',
+    SERVER_PROMPT = '> ',
+    CLIENT_PROMPT = '-> ',
     SERVER_SHUTTING_DOWN = '** server shutting down **',
     CLIENT_CLOSED = '\n--- client closed ---',
-    CLIENT_CONNECTED = '\n--- connected to the chat ---'
+    CLIENT_CONNECTED = '\n--- connected to the chat ---',
 } = process.env
