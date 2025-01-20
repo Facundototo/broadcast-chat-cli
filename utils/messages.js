326227f7a337014904a9f588d5241a9d04790811
Facundototo <facutoto666@gmail.com>
2025-01-20 17:40:45 -0300

    new messages

--- utils/messages.js
+++ utils/messages.js
@@ -1,7 +1,11 @@
 export const {
     SERVER_PROMPT = '> ',
     CLIENT_PROMPT = '-> ',
+    SERVER_NAME_ALERT = 'You do not have a username, type "help"',
     SERVER_SHUTTING_DOWN = '** server shutting down **',
+    SERVER_CLIENT_DISCONNECTED = 'client disconnected:',
+    SERVER_CLIENT_CONNECTED = 'client connected!',
+    SERVER_CLIENT_NAME = 'name setted succesufully',
     CLIENT_CLOSED = '\n--- client closed ---',
     CLIENT_CONNECTED = '\n--- connected to the chat ---',
 } = process.env
