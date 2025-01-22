813b78e560fef2d211d04ed74f977d702bc4977d
Facundototo <facutoto666@gmail.com>
2025-01-22 12:01:48 -0300

    changing messages and prompts

--- utils/messages.js
+++ utils/messages.js
@@ -1,11 +1,11 @@
 export const {
-    SERVER_PROMPT = '> ',
+    SERVER_PROMPT = '[SERVER] ',
     CLIENT_PROMPT = '-> ',
     SERVER_NAME_ALERT = 'You do not have a username, type "help"',
     SERVER_SHUTTING_DOWN = '** server shutting down **',
-    SERVER_CLIENT_DISCONNECTED = 'client disconnected:',
-    SERVER_CLIENT_CONNECTED = 'client connected!',
-    SERVER_CLIENT_NAME = 'name setted succesufully',
+    SERVER_CLIENT_DISCONNECTED = 'client disconnected',
+    SERVER_CLIENT_CONNECTED = 'client connected',
+    SERVER_CLIENT_NAME = 'name setted successfully',
     CLIENT_CLOSED = '\n--- client closed ---',
     CLIENT_CONNECTED = '\n--- connected to the chat ---',
 } = process.env
