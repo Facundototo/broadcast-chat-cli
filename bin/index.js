5e2601e80c8867c117e6e197f70580d627388f36
Facundototo <facutoto666@gmail.com>
2025-01-22 11:59:56 -0300

    -n / --name option done

--- bin/index.js
+++ bin/index.js
@@ -4,12 +4,15 @@ import { connectToServer } from '../lib/client.js';
 import { startServer } from '../lib/server.js';
 
 const args = process.argv.slice(2);
+const hasUsername = (args[1] === '-n' || args[1] === '--name');
 
 switch(args[0]){
+    // broadcast-chat-cli start
     case 'start':
         startServer();
         break;
+    // broadcast-chat-cli connect [-n,--name] [username]
     case 'connect':
-        connectToServer();
+        connectToServer(hasUsername ? args[2] : undefined );
         break;
 }
