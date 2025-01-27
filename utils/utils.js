5c7d8761165a33c3d5c1b77ce95183340e15e749
Facundototo <facutoto666@gmail.com>
2025-01-27 11:47:29 -0300

    i put the commands here, as enums for your reuse

--- utils/utils.js
+++ utils/utils.js
@@ -1,13 +1,23 @@
 export const {
-    SERVER_PROMPT = '[SERVER] ',
     CLIENT_PROMPT = '-> ',
+    CLIENT_CLOSED = '\n--- client closed ---',
+    CLIENT_CONNECTED = '\n--- connected to the chat ---',
+
+    SERVER_PROMPT = '[SERVER] ',
     SERVER_NAME_ALERT = 'You do not have a username, type "help"',
     SERVER_SHUTTING_DOWN = '** server shutting down **',
     SERVER_CLIENT_DISCONNECTED = 'client disconnected',
     SERVER_CLIENT_CONNECTED = 'client connected',
     SERVER_SUCCESSFUL_NAME = 'name setted successfully',
     SERVER_NO_SUCCESSFUL_NAME = 'a client already has that name or it is not valid',
-    CLIENT_CLOSED = '\n--- client closed ---',
-    CLIENT_CONNECTED = '\n--- connected to the chat ---',
-    HELP_ADVICE = 'check the "help" command to use the chat correctly'
+    SERVER_PRIVATE_MESSAGE_ERROR = 'the user you want to send a private message does not exist',
+    
+    HELP_ADVICE = 'check the "help" command to use the chat correctly',
+
+    SET_NAME_COMMAND = '/set-name',
+    SEND_COMMAND = '/send',
+    SEND_TO_COMMAND = '/send-to',
+    HELP_COMMAND = '/help',
+    CLOSE_COMMAND = '/close'
+
 } = process.env
