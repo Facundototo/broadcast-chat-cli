dab842b6e0ed0e7fd02dac0f6e5478013c912c39
Facundototo <facutoto666@gmail.com>
2025-01-29 15:00:50 -0300

    utils.js -> messages.js,printChatChannel() made and two new messages

---
+++ utils/messages.js
@@ -0,0 +1,31 @@
+export const {
+    CLIENT_PROMPT = '-> ',
+    CLIENT_CLOSED = '\n--- client closed ---',
+
+    SERVER_PROMPT = '[SERVER] ',
+    SERVER_NAME_ALERT = 'You do not have a username, type "help"',
+    SERVER_SHUTTING_DOWN = '** server shutting down **',
+    SERVER_CLIENT_DISCONNECTED = 'client disconnected',
+    SERVER_CLIENT_CONNECTED = 'client connected',
+    SERVER_SUCCESSFUL_NAME = 'name setted successfully',
+    SERVER_NO_SUCCESSFUL_NAME = 'a client already has that name or it is not valid',
+    SERVER_PRIVATE_MESSAGE_ERROR = 'the user you want to send a private message does not exist',
+    SERVER_CHANNEL_NOT_FINDED = 'the channel you are trying to join does not exist',
+    SERVER_CHANNEL_JOINED = 'joined to the channel',
+    
+    HELP_ADVICE = 'check the "/help" command to use the chat correctly',
+
+    SET_NAME_COMMAND = '/set-name',
+    SEND_COMMAND = '/send',
+    SEND_TO_COMMAND = '/send-to',
+    JOIN_CHANNEL_COMMAND = '/join',
+    HELP_COMMAND = '/help',
+    CLOSE_COMMAND = '/close',
+    CLEAR_COMMAND = '/clear'
+
+} = process.env
+
+export const printChatChannel = (channel,prompt) => {
+    prompt('');
+    console.log(`--- connected to ${channel} channel ---`);
+}
